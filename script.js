e5c0ddc580e60c9f8a9725ba1a3fe073a41e68b9
Rudrarapu Haarika <haarikarudrarapu@gmail.com>
2026-09-10 00:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+const list = document.getElementById('list');
+let currentItem = 1;
 
+function addItems(count) {
+  for (let i = 0; i < count; i++) {
+    const li = document.createElement('li');
+    li.textContent = `Item ${currentItem}`;
+    list.appendChild(li);
+    currentItem++;
+  }
+}
+
+// 1. Add 25 by default
+addItems(25);
+
+// 2. When user reaches end, add more automatically
+list.addEventListener('scroll', () => {
+  // if scrolled to bottom ( -5px buffer )
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight - 5) {
+    addItems(10);
+  }
+});
